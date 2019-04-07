45614c771c49dbf717a21a2bbdcc88ca0011fdab
jay.feng <jay.feng@sainsburys.co.uk>
2019-04-07 12:28:48 +0100

    Initial commit

---
+++ src/test/java/com/sainsburys/imagefinder/ImageFinderApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sainsburys.imagefinder;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImageFinderApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
